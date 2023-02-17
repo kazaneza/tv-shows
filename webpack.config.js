d36514d52dc4d6d2099d15cd66c3696d2b885188
kazaneza <kazanezagentil@gmail.com>
2023-02-17 23:31:23 +0100

    finilizing project

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  devServer: {
-    static: './dist',
-  },
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = {
   entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
@@ -15,22 +21,26 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
     ],
   },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production';
+  } else {
+    config.mode = 'development';
+  }
+  return config;
 };
